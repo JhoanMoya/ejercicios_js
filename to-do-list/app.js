0db02169ef681a26fa71661b9b333c35f0f4fca1
TheMomo <130094214+JhoanMoya@users.noreply.github.com>
2025-02-28 12:46:52 -0500

    He actualizado el codigo en los archivos app.js, index.html y styles.css

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,22 +1,21 @@
-document.getElementById('addTaskBtn').addEventListener('click', function() {
+document.getElementById('addTaskBtn').addEventListener('click', function () {
     const taskInput = document.getElementById('taskInput');
-    const task = taskInput.value.trim();
+    const taskText = taskInput.value.trim();
 
     if (taskText !== '') {
         addTask(taskText);
         taskInput.value = '';
     }
-
 });
 
 function addTask(taskText) {
     const taskList = document.getElementById('taskList');
     const li = document.createElement('li');
-    li.innerHTML = taskText;
+    li.textContent = taskText;
 
     const deleteBtn = document.createElement('button');
-    delateBtn.textContent = 'Eliminar';
-    deleteBtn.addEventListener('click', function() {
+    deleteBtn.textContent = 'Eliminar';
+    deleteBtn.addEventListener('click', function () {
         taskList.removeChild(li);
     });
 
